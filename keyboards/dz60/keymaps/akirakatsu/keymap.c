05a7f6bbc712cb714a8be9f84c1969baf5098de2
Patrick de Ritter <patrickderitter@users.noreply.github.com>
2021-04-12 21:50:49 +0000

    add debug

--- keyboards/dz60/keymaps/akirakatsu/keymap.c
+++ keyboards/dz60/keymaps/akirakatsu/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_AUDIO_MUTE,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCREEN, _______, _______, KC_BSLASH,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END, _______, KC_AUDIO_VOL_UP, _______,
+        _______, _______, _______, _______, _______, _______,   DEBUG, _______, _______, KC_HOME, KC_END, _______, KC_AUDIO_VOL_UP, _______,
         _______, _______, _______, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PLAY_PAUSE, KC_CALCULATOR, KC_MEDIA_PREV_TRACK, _______, KC_AUDIO_VOL_DOWN, KC_MEDIA_NEXT_TRACK
     )
 };

6fdee39ff051c01f1e105c10adc4b971aeb465f1
Patrick de Ritter <patrickderitter@users.noreply.github.com>
2021-04-24 21:18:58 +0200

    planck keymap

---
+++ keyboards/planck/keymaps/patrickderitter/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PLANCK_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+#endif
+
+// Most tactile encoders have detents every 4 stages
+#define ENCODER_RESOLUTION 4
+
